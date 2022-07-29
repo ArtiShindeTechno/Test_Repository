5df3b6cd5eb5f33956f6c10fa3fbe76659662de1
Arti Shinde <arti.shinde89@gmail.com>
2022-07-29 17:04:37 +0530

    Updated from Arti_branch3

--- src/Test1.java
+++ src/Test1.java
@@ -3,9 +3,9 @@ public class Test1 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Test Project updated on Arti_branch1");
-		System.out.println("Test Project");
-		System.out.println("Upated from Arti_branch2");
+		System.out.println("Test Project updated on Arti_branch1==updated from Arti_branch3");
+		System.out.println("Test Project==updated from Arti_branch3");
+		System.out.println("Upated from Arti_branch2==updated from Arti_branch3");
 		System.out.println("Arti_branch2...");
 	}
 }
